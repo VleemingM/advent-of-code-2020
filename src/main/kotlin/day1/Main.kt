2cb502ea0a8e973e74bb1cb09eab5c0cbd81b6fb
Michiel Vleeming <Michiel.vleeming@nos.nl>
2022-01-25 20:59:52 +0100

    day2 solution

--- src/main/kotlin/day1/Main.kt
+++ src/main/kotlin/day1/Main.kt
@@ -5,15 +5,24 @@ import java.io.File
 
 fun main(args: Array<String>) {
     val numbers = readFile()
-    numbers.forEach { firstNumber ->
-        numbers.forEach { secondNumber ->
-            if (firstNumber + secondNumber == 2020){
-                println(firstNumber * secondNumber)
-            }
+    val triple =numbers.findTripleOfSum()
+    println(triple?.let { (x, y, z) -> x * y * z} )
+}
+fun List<Int>.findPairOfSum(sum: Int): Pair<Int, Int>? {
+    // Map: sum - x -> x
+    val complements = associateBy { sum - it }
+    return firstNotNullOfOrNull { number ->
+        val complement = complements[number]
+        if (complement != null) Pair(number, complement) else null
+    }
+}
+
+fun List<Int>.findTripleOfSum(): Triple<Int, Int, Int>? =
+    firstNotNullOfOrNull { x ->
+        findPairOfSum(2020 - x)?.let { pair ->
+            Triple(x, pair.first, pair.second)
         }
     }
-
-}
 
 fun readFile() :List<Int>{
     val numbers = mutableListOf<Int>()
--- src/main/kotlin/day2/Main.kt
+++ src/main/kotlin/day2/Main.kt
@@ -4,24 +4,57 @@ import java.io.File
 
 
 fun main(args: Array<String>) {
-    val numbers = readFile()
-    numbers.forEach { firstNumber ->
-        numbers.forEach { secondNumber ->
-            numbers.forEach { thirdNumber ->
-                if (firstNumber + secondNumber + thirdNumber == 2020) {
-                    println(firstNumber * secondNumber * thirdNumber)
-                }
-            }
+    val data = readFile()
+    println(data)
+
+    partOne(data)
+    partTwo(data)
+
+
+}
+
+fun partOne(data: List<PasswordValidity>) {
+    val validPasswords = data.count {
+        it.password.count { s -> s == it.requiredLetter }.let { numberOfRequiredLetters ->
+            numberOfRequiredLetters >= it.minNumber && numberOfRequiredLetters <= it.maxNumber
         }
     }
-
+    println(validPasswords)
 }
 
-fun readFile(): List<Int> {
-    val numbers = mutableListOf<Int>()
-    File("src/main/resources/day1/input.txt").forEachLine {
+fun partTwo(data: List<PasswordValidity>) {
+    println(data.count {
+        val firstIsValid = it.password[it.minNumber - 1] == it.requiredLetter
+        val secondIsValid = it.password[it.maxNumber - 1] == it.requiredLetter
+        if (firstIsValid) {
+            !secondIsValid
+        } else {
+            secondIsValid
+        }
+    })
+}
 
-        numbers.add(Integer.parseInt(it))
+fun readFile(): List<PasswordValidity> {
+    val data = mutableListOf<PasswordValidity>()
+    File("src/main/resources/day2/input.txt").forEachLine {
+
+        val minNumber = it.split(":", " ", "-")
+
+        data.add(
+            PasswordValidity(
+                Integer.parseInt(minNumber[0]),
+                Integer.parseInt(minNumber[1]),
+                minNumber[2].first(),
+                minNumber[4]
+            )
+        )
     }
-    return numbers
+    return data
 }
+
+data class PasswordValidity(
+    val minNumber: Int,
+    val maxNumber: Int,
+    val requiredLetter: Char,
+    val password: String,
+)
